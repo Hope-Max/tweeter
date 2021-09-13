34f050cfb82d9f53620dc5ccb84fcd0b42c2b675
Max Li <hope_max@outlook.com>
2021-09-13 17:03:50 -0400

    Add the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on('input', function() {
+    const $input = $(this).val();
+    const length = $input.length;
+    const initial = 140;
+    const $counter = initial - length;
+
+    // find the CSS selector which mataches the .counter by using jQuery and keyword this
+    const $outputElement = $(this).next().find('.counter');
+    $outputElement.val($counter);
+
+    // the color is black when the counter is greater than or equal to 0, red otherwise
+    $outputElement.css('color', 'black');
+    if ($counter < 0) {
+      $outputElement.css('color', 'red');
+    }
+
+  });
+});
