3e9a9cad4b50386d4f8f8d0bd437c7248068f518
Max Li <hope_max@outlook.com>
2021-09-16 21:39:59 -0400

    Add the functionality of posting the tweet text to the server by using jQuery and AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,26 +6,52 @@
 
 $(document).ready(function() {
 
-  const createTweetElement = function(tweetObject) {
-    return `
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const createTweetElement = function(tweet) {
+
+    const $tweet = $(`
       <article>
         <header class="header-tweet">
           <div class="favicon">
             <div>
-              <img class="img" src=${tweetObject.user.avatars}> 
+              <img class="img" src=${tweet.user.avatars}> 
             </div>
             <div>
-              <p class="name">${tweetObject.user.name}</p>
+              <p class="name">${tweet.user.name}</p>
             </div>
           </div>
           <div class="handle">
-            <p>${tweetObject.user.handle}</p>
+            <p>${tweet.user.handle}</p>
           </div>
         </header>
-        <textarea class="text-tweet" name="text">${tweetObject.content.text}</textarea>
+        <textarea class="text-tweet" name="text">${tweet.content.text}</textarea>
         <footer class="footer-tweet">
           <div class="timestamp">
-            <p>${tweetObject.created_at}</p>
+            <p>${timeago.format(tweet.created_at)}</p>
           </div>
           <div class="logo">
             <i class="fas fa-flag"></i>
@@ -34,22 +60,36 @@ $(document).ready(function() {
           </div>
         </footer>
       </article>
-    `
+    `);
+
+    return $tweet;
   };
   
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1631577925118
+  const renderTweets = function(tweets) {
+
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#sub-container').append($tweet);
+    }
+
   };
-  
-  const $tweet = createTweetElement(tweetData);
-  $('#sub-container').append($tweet);
+
+  renderTweets(data);
+
+  // Add an event listener for sumbit and prevent its default behaviour
+  $('form').submit(function(event) {
+    event.preventDefault();
+
+    const $form = $(this);
+    console.log($form);
+
+    $.ajax({
+      url: '/',
+      type: 'POST',
+      data: $form.serialize(),
+    });
+
+    console.log($form.serialize());
+  })
 
 });
